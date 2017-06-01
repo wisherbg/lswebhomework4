ee742ec93cd03491e10c0407ab288780a1ce7908
WisherBG <wisher@abv.bg>
2017-06-01 15:43:06 +0300

    Added jQuery version. TODO: Add picture with jQuery

---
+++ samplejquery/script.js
@@ -0,0 +1,8 @@
+$('body').css('background-color','red');
+$('body').css('font-family', 'sans-serif');
+$('h1').css('background-color','blue');
+$('li').css('background-color', 'white');
+$('li').css('color', 'red');
+$('#nickname').text('WisherBG');
+$('#favorites').text('Lambda School');
+$('#hometown').text('Not saying');
