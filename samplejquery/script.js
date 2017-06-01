fde0d7c13d3bc00150443f72a928486d03020389
WisherBG <wisher@abv.bg>
2017-06-01 16:21:04 +0300

    added image to jQuery project

--- samplejquery/script.js
+++ samplejquery/script.js
@@ -1,5 +1,6 @@
 $('body').css('background-color','red');
 $('body').css('font-family', 'sans-serif');
+$('body').append('<img src="https://qph.ec.quoracdn.net/main-qimg-c0f2e7c8e5fb40c52acd389e5de0d314" />');
 $('h1').css('background-color','blue');
 $('li').css('background-color', 'white');
 $('li').css('color', 'red');
